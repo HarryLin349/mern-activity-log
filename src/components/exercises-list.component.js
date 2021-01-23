b4da8f735c0d4e95521108ca221522e1f38d6cd4
Harry Lin <harrylin349@gmail.com>
2021-01-23 16:11:30 -0500

    update pathing to api

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -24,7 +24,7 @@ export default class ExercisesList extends Component {
   }
 
   componentDidMount() {
-    axios.get('/exercises/')
+    axios.get('/api/exercises/')
       .then(response => {
         this.setState({ exercises: response.data })
       })
@@ -34,7 +34,7 @@ export default class ExercisesList extends Component {
   }
 
   deleteExercise(id) {
-    axios.delete('/exercises/'+id)
+    axios.delete('/api/exercises/'+id)
       .then(response => { console.log(response.data)});
 
     this.setState({
--- src/index.js
+++ src/index.js
@@ -2,4 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+const proxy = require('http-proxy-middleware')
+
+export default function(app) {
+    // add other server routes to path array
+    app.use(proxy(['/api' ], { target: 'http://localhost:5000' }));
+} 
+
 ReactDOM.render(<App/>, document.getElementById('root'));
