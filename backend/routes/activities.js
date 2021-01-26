d9786396029ce59f0dff25ed003a11fdaed1bb6c
Harry Lin <harrylin349@gmail.com>
2021-01-25 19:53:08 -0500

    change internal variables

--- backend/routes/activities.js
+++ backend/routes/activities.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
-let Exercise = require('../models/exercise.model');
+let Activity = require('../models/activity.model');
 
 router.route('/').get((req, res) => {
-  Exercise.find()
-    .then(exercises => res.json(exercises))
+  Activity.find()
+    .then(activities => res.json(activities))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
@@ -13,40 +13,40 @@ router.route('/add').post((req, res) => {
   const duration = Number(req.body.duration);
   const date = Date.parse(req.body.date);
 
-  const newExercise = new Exercise({
+  const newActivity = new Activity({
     username,
     description,
     duration,
     date,
   });
 
-  newExercise.save()
-  .then(() => res.json('Exercise added!'))
+  newActivity.save()
+  .then(() => res.json('Activity added!'))
   .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/:id').get((req, res) => {
-  Exercise.findById(req.params.id)
-    .then(exercise => res.json(exercise))
+  Activity.findById(req.params.id)
+    .then(activity => res.json(activity))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/:id').delete((req, res) => {
-  Exercise.findByIdAndDelete(req.params.id)
-    .then(() => res.json('Exercise deleted.'))
+  Activity.findByIdAndDelete(req.params.id)
+    .then(() => res.json('Activity deleted.'))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/update/:id').post((req, res) => {
-  Exercise.findById(req.params.id)
-    .then(exercise => {
-      exercise.username = req.body.username;
-      exercise.description = req.body.description;
-      exercise.duration = Number(req.body.duration);
-      exercise.date = Date.parse(req.body.date);
+  Activity.findById(req.params.id)
+    .then(activity => {
+      activity.username = req.body.username;
+      activity.description = req.body.description;
+      activity.duration = Number(req.body.duration);
+      activity.date = Date.parse(req.body.date);
 
-      exercise.save()
-        .then(() => res.json('Exercise updated!'))
+      activity.save()
+        .then(() => res.json('Activity updated!'))
         .catch(err => res.status(400).json('Error: ' + err));
     })
     .catch(err => res.status(400).json('Error: ' + err));
---
+++ src/components/activities-list.component.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+const serverURL = process.env.PORT || "http://localhost:5000"
+const Activity = props => (
+  <tr>
+    <td>{props.activity.username}</td>
+    <td>{props.activity.description}</td>
+    <td>{props.activity.duration}</td>
+    <td>{props.activity.date.substring(0,10)}</td>
+    <td>
+      <Link to={"/edit/"+props.activity._id}>edit</Link> | <a href="#" onClick={() => { props.deleteActivity(props.activity._id) }}>delete</a>
+    </td>
+  </tr>
+)
+
+export default class ActivitiesList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.deleteActivity = this.deleteActivity.bind(this)
+
+    this.state = {activities: []};
+  }
+
+  componentDidMount() {
+    axios.get(serverURL + '/activities/')
+      .then(response => {
+        this.setState({ activities: response.data })
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+  }
+
+  deleteActivity(id) {
+    axios.delete(serverURL + '/activities/'+id)
+      .then(response => { console.log(response.data)});
+
+    this.setState({
+      activities: this.state.activities.filter(el => el._id !== id)
+    })
+  }
+
+  activityList() {
+    return this.state.activities.map(currentactivity => {
+      return <Activity activity={currentactivity} deleteActivity={this.deleteActivity} key={currentactivity._id}/>;
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Activity Log</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration (minutes)</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.activityList() }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+}
